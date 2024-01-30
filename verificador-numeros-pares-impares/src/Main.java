6a7ea50b90593eacb37c576cd90fca265804ed9c
dev-leomadeira <149954966+devleo-m@users.noreply.github.com>
2024-01-30 13:51:07 -0300

    inicial commit project

---
+++ verificador-numeros-pares-impares/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+//**
+// Verificador de Números Pares ou Ímpares:
+// Implemente um programa que solicita ao usuário um número e verifica se é par ou ímpar.
+// **/
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um numero: ");
+        int numero = scanner.nextInt();
+        if (numero % 2 == 0){
+            System.out.println(numero+" Par");
+        }else {
+            System.out.println(numero+" Ímpar");
+        }
+    }
+}
