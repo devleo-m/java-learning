34da0497b87190c4c3e1caec64051869f5bc6baa
dev-leomadeira <149954966+devleo-m@users.noreply.github.com>
2024-01-30 13:55:43 -0300

    atividade numeroParImpar

--- verificador-numeros-pares-impares/src/Main.java
+++ verificador-numeros-pares-impares/src/Main.java
@@ -7,8 +7,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Digite um numero: ");
         int numero = scanner.nextInt();
+        
         if (numero % 2 == 0){
             System.out.println(numero+" Par");
         }else {
