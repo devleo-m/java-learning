0f84375e76dddf91a106cc2ed3a4ed4018169beb
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-02-21 23:53:56 -0300

    Update GerenciadorTarefas.java

--- ListaDeTarefas/src/GerenciadorTarefas.java
+++ ListaDeTarefas/src/GerenciadorTarefas.java
@@ -2,26 +2,35 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GerenciadorTarefas {
-    ArrayList<String> tarefas = new ArrayList<>();
-    public void adicionarTarefa(){
-        Scanner scanner = new Scanner(System.in);
+    static ArrayList<String> tarefas = new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void adicionarTarefa(){
         String value = scanner.next();
         tarefas.add(value);
     }
-    public void exibirTarefa(){
-        for (int i = 0; i < tarefas.size(); i++) {
-            System.out.println(i+": "+tarefas.get(i));
+    public static void exibirTarefa(){
+        if (tarefas.isEmpty()){
+            System.out.println("A lista esta vazia");
+        } else {
+            for (int i = 0; i < tarefas.size(); i++) {
+                System.out.println(i+": "+tarefas.get(i));
+            }
         }
     }
-    public void removerTarefa() {
+    public static void removerTarefa() {
+        exibirTarefa();
 
-    }
+        if (!tarefas.isEmpty()){
+            System.out.print("Digite o numero da tarefa p/ ser removida: ");
+            int numTarefa = scanner.nextInt();
 
-    public ArrayList<String> getTarefas() {
-        return tarefas;
-    }
-
-    public void setTarefas(ArrayList<String> tarefas) {
-        this.tarefas = tarefas;
+            if (numTarefa > 0 && numTarefa <= tarefas.size()){
+                String tarefaRemovida = tarefas.remove(numTarefa - 1);
+                System.out.println("Tarefa \"" + tarefaRemovida + "\" removida com sucesso!");
+            } else {
+                System.out.println("Número de tarefa inválido.");
+            }
+        }
     }
 }
